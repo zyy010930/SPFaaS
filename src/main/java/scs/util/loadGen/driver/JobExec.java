84c57327c6d81aa729805a675780c0ccf0bba315
zyy <1755635168@qq.com>
2024-01-09 22:38:57 +0800

    version1.9.6

--- src/main/java/scs/util/loadGen/driver/JobExec.java
+++ src/main/java/scs/util/loadGen/driver/JobExec.java
@@ -45,8 +45,8 @@ public class JobExec {
         System.out.println(ConfigPara.funcName[serviceId-1] + " Invoke time is " + ConfigPara.invokeTime[serviceId-1] + ", cold start time is " + ConfigPara.coldStartTime[serviceId-1] + ", cold start rate is " + ((double)ConfigPara.coldStartTime[serviceId-1]/ConfigPara.invokeTime[serviceId-1])*100.0 + "%, preWarm time is " + ConfigPara.preWarm[serviceId-1] + ", keepAive time is " + ConfigPara.keepAlive[serviceId-1]);
         ConfigPara.funcFlagArray[serviceId-1] = 1;
 
-        Thread.sleep((long) ConfigPara.keepAlive[serviceId-1]);
         int lastTime = ConfigPara.invokeTime[serviceId-1];
+        Thread.sleep((long) ConfigPara.keepAlive[serviceId-1]);
         if(ConfigPara.funcFlagArray[serviceId-1] == 1 && ConfigPara.invokeTime[serviceId-1] == lastTime) //如果期间没有新的调用，就直接释放
         {
             ConfigPara.funcFlagArray[serviceId-1] = 0;
