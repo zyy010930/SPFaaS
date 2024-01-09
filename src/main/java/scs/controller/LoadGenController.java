2b0efe23ce8ecbc34ffdcb96e5b39039a71102a6
zyy <1755635168@qq.com>
2024-01-09 23:12:47 +0800

    version1.9.8

--- src/main/java/scs/controller/LoadGenController.java
+++ src/main/java/scs/controller/LoadGenController.java
@@ -243,10 +243,10 @@ public class LoadGenController {
 		public void run(){
 			System.out.println("new start");
 			//SPFaaS
-			PreWarmThread thread = new PreWarmThread(serviceId);
-			ExecutorService executorService = Executors.newFixedThreadPool(1);
-			executorService.execute(thread);
-			executorService.shutdown();
+//			PreWarmThread thread = new PreWarmThread(serviceId);
+//			ExecutorService executorService = Executors.newFixedThreadPool(1);
+//			executorService.execute(thread);
+//			executorService.shutdown();
 			for(Integer time : this.list)
 			{
 				System.out.println("function:" + serviceId + "sleep:" + time);
@@ -317,7 +317,7 @@ public class LoadGenController {
 		}
 		public void run() {
 			try {
-				OverFramework.run(this.serviceId,6);
+				OverFramework.run(this.serviceId,3);
 			} catch (InterruptedException e) {
 				throw new RuntimeException(e);
 			}
@@ -358,8 +358,8 @@ public class LoadGenController {
 //					ConfigPara.setMemoryCapacity(30000.0 - d);
 //				}
 			}
-			//String csvFilePath = "/home/zyy/SPFaaS_memory_100.csv";
-			String csvFilePath = "/home/zyy/SPFaaS_memory100.csv";
+			//String csvFilePath = "/home/zyy/SPFaaS_memory100.csv";
+			String csvFilePath = "/home/zyy/Wild_memory.csv";
 			try {
 				BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
 				writer.write("memory\n");
@@ -375,8 +375,8 @@ public class LoadGenController {
 //				System.out.println(list.get(i));
 //			}
 
-			//String csvFilePath1 = "/home/zyy/SPFaaS_rate_100.csv";
-			String csvFilePath1 = "/home/zyy/SPFaaS_rate100.csv";
+			//String csvFilePath1 = "/home/zyy/SPFaaS_rate100.csv";
+			String csvFilePath1 = "/home/zyy/Wild_rate.csv";
 			try {
 				BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath1));
 				writer.write("invoke,coldStart,rate\n");
