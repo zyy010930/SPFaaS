c1c58289543971188a44e99f9f93c65446372f9a
zyy <1755635168@qq.com>
2024-01-16 09:55:23 +0800

    version1.16.1

--- src/main/java/scs/controller/ConfigPara.java
+++ src/main/java/scs/controller/ConfigPara.java
@@ -63,7 +63,7 @@ public class ConfigPara {
 
     public ConfigPara() {
         //maxFuncCapacity = 43500.0;
-        beta = 0.9;
+        beta = 0.2;
         gama = 0.5;
         maxFuncCapacity = 22500.0;
         currFuncCapacity = 0.0;
