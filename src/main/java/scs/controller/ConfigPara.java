f15c1841047c185f10f88d6c23ebf7baf344081b
zyy <1755635168@qq.com>
2025-05-15 17:15:52 +0800

    2025.5.15.2

--- src/main/java/scs/controller/ConfigPara.java
+++ src/main/java/scs/controller/ConfigPara.java
@@ -67,7 +67,7 @@ public class ConfigPara {
     public static Integer[] preWarmTime;
 
     public ConfigPara() {
-        maxFuncCapacity = 24000.0;
+        maxFuncCapacity = 22500.0;
         beta = 0.5;
         gama = 0.5;
         //maxFuncCapacity = 22500.0;
