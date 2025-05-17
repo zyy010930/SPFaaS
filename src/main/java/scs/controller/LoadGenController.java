6a64e2b4d5edb07f5dcc996f702a910e5819520f
zyy <1755635168@qq.com>
2025-05-17 19:53:28 +0800

    5.17.4

--- src/main/java/scs/controller/LoadGenController.java
+++ src/main/java/scs/controller/LoadGenController.java
@@ -364,7 +364,7 @@ public class LoadGenController {
 //					ConfigPara.setMemoryCapacity(30000.0 - d);
 //				}
 			}
-			String csvFilePath = "/home/zyy/SPFaaS_memoryβ0.3.csv";
+			String csvFilePath = "/home/zyy/SPFaaS_memoryβ0.4.csv";
 			//String csvFilePath = "/home/zyy/Ice_memory_new.csv";
 			try {
 				BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath));
@@ -381,7 +381,7 @@ public class LoadGenController {
 //				System.out.println(list.get(i));
 //			}
 
-			String csvFilePath1 = "/home/zyy/SPFaaS_rateβ0.3.csv";
+			String csvFilePath1 = "/home/zyy/SPFaaS_rateβ0.4.csv";
 			//String csvFilePath1 = "/home/zyy/Ice_rate_new.csv";
 			try {
 				BufferedWriter writer = new BufferedWriter(new FileWriter(csvFilePath1));
